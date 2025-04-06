01803d4cd2b8a9014b59e8d18c8d2f46bbf10695
Cocomong98 <yonghjjm0618@gmail.com>
2025-04-06 23:53:43 +0900

    DFS

---
+++ DFS/a.js
@@ -0,0 +1,21 @@
+let maze = [
+    ["S", 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1],
+    [0, 0, 0, 0, "E"],
+    [1, 1, 1, 0, 1],
+];
+
+function dfs(maze, position = [0, 0], path = []) {
+    let [x, y] = position;
+    // 종점에 다다르면 해당 경로 리턴 (정답)
+    if (maze[x][y] === "E") return [...path, position];
+
+    // 상하좌우 이동
+    let directions = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+    ];
+}
